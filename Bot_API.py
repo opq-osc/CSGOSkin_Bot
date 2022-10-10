1d88fbaa14da68cb53b1b7361f85149ffd004d27
Lord2333 <50525597+Lord2333@users.noreply.github.com>
2022-10-10 16:18:22 +0800

    Add files via upload
    使用Botoy自带jconfig函数引入脚本所需全部参数。

---
+++ Bot_API.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template
+import Func
+
+func = Func.CSGO()
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET"])
+def index():
+	print(request.headers.get('User-Agent'))
+	return render_template('./index.html', flag=1)
+
+
+@app.route("/<string:short_code>", methods=['GET'])
+def deal_url(short_code):
+	QQ = func.Chk_API(short_code)
+	if QQ:
+		return render_template('./list.html', Code=short_code, QQ=QQ)
+	else:
+		return render_template('./index.html', flag=0)
+
+
+@app.route("/db/<string:short_code>", methods=['GET'])
+def get_data(short_code):
+	return func.Get_data(short_code)
+
+
+if __name__ == '__main__':
+	app.run(debug=False, port=7355, threaded=True)
---
+++ bot.py
@@ -0,0 +1,39 @@
+import asyncio, json
+
+from botoy import AsyncBotoy, FriendMsg, jconfig
+from botoy.decorators import equal_content, ignore_botself
+from botoy.sugar import Text
+
+bot = AsyncBotoy(
+    host=str(jconfig.host),
+    port=int(jconfig.port),
+    log=True,
+    log_file=True,
+    use_plugins=True,
+)
+
+
+@bot.friend_context_use
+def friend_ctx_middleware(ctx: FriendMsg):
+    ctx.QQ = ctx.FromUin  # 这条消息的发送者
+    if ctx.MsgType == "TempSessionMsg":  # 临时会话
+        ctx.Content = json.loads(ctx.Content)["Content"]
+        ctx.type = "temp"
+        ctx.QQG = ctx.TempUin
+    else:
+        ctx.type = "friend"  # 好友会话
+        ctx.QQG = 0
+    return ctx
+
+
+@bot.on_group_msg
+@bot.on_friend_msg
+@ignore_botself
+@equal_content("帮助")
+def help(_):
+    Text(bot.plugMgr.help)
+
+
+if __name__ == "__main__":
+    asyncio.run(bot.run())
+
